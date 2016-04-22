ca6a353ffa84d9adaa3a2b3a7749c3ccea0a8cea
ly.jy <ly.jy@G08JYZSD150039.g08.fujitsu.local>
2016-04-22 14:42:16 +0800

    业务逻辑层更新

--- src/com/fujitsu/memo/Acitivity/MainActivity.java
+++ src/com/fujitsu/memo/Acitivity/MainActivity.java
@@ -1,19 +1,12 @@
 package com.fujitsu.memo.Acitivity;
 
-import android.support.v4.app.Fragment;
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.Window;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
-import android.os.Build;
 
 public class MainActivity extends Activity implements OnClickListener {
 
