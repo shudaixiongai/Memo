9006f40943ba06288c4ef7a24189c78220cd7cca
shudaixiongai <122624407@qq.com>
2016-04-22 16:08:01 +0800

    test

---
+++ src/com/fujitsu/memo/test/DBMangerTest.java
@@ -0,0 +1,18 @@
+package com.fujitsu.memo.test;
+
+import com.fujitsu.memo.DB.DBManger;
+import com.fujitsu.memo.model.Memo;
+
+import junit.framework.TestCase;
+
+public class DBMangerTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	public void testAddMemo() {
+		Memo memo;
+		DBManger dbManger = new DBManger();
+	}
+}
