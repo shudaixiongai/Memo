4976359275bca479246f5a904ea9631564a47aa8
ly.jy <ly.jy@G08JYZSD150039.g08.fujitsu.local>
2016-04-27 15:39:27 +0800

    添加详情界面

---
+++ src/com/fujitsu/memo/Acitivity/DetailsActivity.java
@@ -0,0 +1,14 @@
+package com.fujitsu.memo.Acitivity;
+
+import android.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DetailsActivity extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView();
+	}
+}
